b23b1db670e2ee7ee64bafd6b7be6a7bb638aed1
Salman Nazeer <salmannazeer477@gmail.com>
2026-01-18 11:39:05 +0530

    Reformatted document

--- array_traversal.c
+++ array_traversal.c
@@ -1,19 +1,19 @@
 #include<stdio.h>
 
 int main(){
-        int array[100], index = -1;
-        int size = 0;
-        printf("Enter size of array:");
-        scanf("%d", &size);
-        printf("Enter array elements:");
-        for(int i = 0; i < size; i++) scanf("%d", &array[i]);
-        int num;
-        printf("Enter number to be searched:");
-	scanf("%d", &num);
-	for(int i = 0; i < size; i++){
-		if(array[i] == num) index = i;
-	}
-	if(index == -1) printf("%d not found in array\n", num);
-	else printf("%d found at index %d\n", num, index);
-        return 0;
+    int array[100], index = -1;
+    int size = 0;
+    printf("Enter size of array:");
+    scanf("%d", &size);
+    printf("Enter array elements:");
+    for(int i = 0; i < size; i++) scanf("%d", &array[i]);
+    int num;
+    printf("Enter number to be searched:");
+    scanf("%d", &num);
+    for(int i = 0; i < size; i++){
+    	if(array[i] == num) index = i;
+    }
+    if(index == -1) printf("%d not found in array\n", num);
+    else printf("%d found at index %d\n", num, index);
+    return 0;
 }
