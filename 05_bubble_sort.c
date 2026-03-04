c3cb0bd082e3a22aa5af53c20acf68c5bcb98b47
Salman Nazeer <salmannazeer477@gmail.com>
2026-03-04 06:49:18 +0530

    Shortened bubblesort code

--- 05_bubble_sort.c
+++ 05_bubble_sort.c
@@ -1,24 +1,17 @@
 #include<stdio.h>
-
-int main()
-{
-    int arr[100];
-    int size = 0;
+int main(){
+    int arr[100], size = 0, temp;
     printf("Enter size of array:");
     scanf("%d", &size);
     printf("Enter array elements:");
-    for (int i = 0; i < size; i++)
-        scanf("%d", &arr[i]);
-    int temp;
-    for(int i = 0; i < size - 1; i++){
-        for(int j = 0; j < size - i - 1; j++){
+    for (int i = 0; i < size; i++) scanf("%d", &arr[i]);
+    for(int i = 0; i < size - 1; i++)
+        for(int j = 0; j < size - i - 1; j++)
             if(arr[j] > arr[j + 1]){
                 temp = arr[j];
                 arr[j] = arr[j + 1];
                 arr[j + 1] = temp;
             }
-        }
-    }
     printf("Sorted Array:\n");
     for(int i = 0; i < size; i++) printf("%d, ", arr[i]);
     printf("\n");
