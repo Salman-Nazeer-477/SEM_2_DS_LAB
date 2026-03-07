203263743e39d84ce0da7d169f0e21b8383325ef
Salman Nazeer <salmannazeer477@gmail.com>
2026-03-07 15:16:51 +0530

    shortened selection sort

--- 06_selection_sort.c
+++ 06_selection_sort.c
@@ -1,23 +1,15 @@
 #include<stdio.h>
-
 int main()
 {
-    int arr[100], index = -1;
-    int size = 0;
+    int arr[100], index = -1, size = 0;
     printf("Enter size of array:");
     scanf("%d", &size);
     printf("Enter array elements:");
-    for (int i = 0; i < size; i++)
-        scanf("%d", &arr[i]);
-    int i_min;
-    int temp;
+    for (int i = 0; i < size; i++) scanf("%d", &arr[i]);
+    int i_min, temp;
     for(int i = 0; i < size - 1; i++){
         i_min = i;
-        for(int j = i + 1; j < size; j++){
-            if(arr[j] < arr[i_min]){
-                i_min = j;
-            }
-        }
+        for(int j = i + 1; j < size; j++) if(arr[j] < arr[i_min]) i_min = j;
         temp = arr[i];
         arr[i] = arr[i_min];
         arr[i_min] = temp;
