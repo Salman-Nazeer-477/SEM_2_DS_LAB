c4c9c2bba5d03d4d7a99666b20736def7ee2c5f0
Salman Nazeer <salmannazeer477@gmail.com>
2026-03-07 15:20:12 +0530

    shortened matrix addition

--- 08_matrix_addition.c
+++ 08_matrix_addition.c
@@ -1,24 +1,15 @@
 #include<stdio.h>
-
 int main(){
-    int A[50][50];
-    int B[50][50];
-    int c, r;
+    int A[50][50], B[50][50], c, r;
     printf("Enter no. of rows and columns of two matrices:");
     scanf("%d %d", &r, &c);
     printf("Enter Matrix A:\n");
-    for(int i = 0; i < r; i++)
-        for(int j = 0; j < c; j++)
-            scanf("%d", &A[i][j]);
+    for(int i = 0; i < r; i++) for(int j = 0; j < c; j++) scanf("%d", &A[i][j]);
     printf("Enter Matrix B:\n");
-    for(int i = 0; i < r; i++)
-        for(int j = 0; j < c; j++)
-            scanf("%d", &B[i][j]);
+    for(int i = 0; i < r; i++) for(int j = 0; j < c; j++) scanf("%d", &B[i][j]);
     printf("Matrix C = A + B:\n");
     for(int i = 0; i < r; i++){
-        for(int j = 0; j < c; j++){
-            printf("%d ", A[i][j] + B[i][j]);
-        }
+        for(int j = 0; j < c; j++) printf("%d ", A[i][j] + B[i][j]);
         printf("\n");
     }
     return 0;
