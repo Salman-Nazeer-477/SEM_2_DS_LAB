58699903523618a31ab872d1b852c7ac1c1a7f97
Salman Nazeer <salmannazeer477@gmail.com>
2026-03-07 15:31:41 +0530

    Shortened linear Stack

--- 11_linear_stack_array.c
+++ 11_linear_stack_array.c
@@ -1,54 +1,32 @@
 #include <stdio.h>
 #define SIZE 10
-int main()
-{
-    int stack[SIZE];
-    int top = -1;
-    int num;
-    int choice;
-    do
-    {
+int main(){
+    int stack[SIZE], top = -1, num, choice;
+    do{
         printf("\n");
         printf("1. Push\n");
         printf("2. Pop\n");
         printf("3. Display\n");
         printf("0. Exit\n");
         scanf("%d", &choice);
-        switch (choice)
-        {
+        switch (choice){
         case 1:
-            if (top + 1 == SIZE)
-            {
-                printf("Stack Overflow!\n");
-            }
-            else
-            {
+            if (top + 1 == SIZE) printf("Stack Overflow!\n");
+            else{
                 printf("Enter number to be pushed:");
                 scanf("%d", &num);
-                top++;
-                stack[top] = num;
+                stack[++top] = num;
             }
             break;
         case 2:
-            if (top == -1)
-            {
-                printf("Stack Underflow!\n");
-            }
-            else
-            {
-                printf("%d", stack[top]);
-                top--;
-            }
+            if (top == -1) printf("Stack Underflow!\n");
+            else printf("Popped: %d", stack[top--]);
             break;
         case 3:
             printf("Stack:\n");
-            for (int i = top; i > -1; i--)
-            {
-                printf("%d ", stack[i]);
-            }
+            for (int i = top; i > -1; i--) printf("%d ", stack[i]);
             printf("\n");
             break;
         }
-
     } while (choice != 0);
 }
