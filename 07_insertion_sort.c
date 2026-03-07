3886b774329842aa38504bea380965227e8d7b23
Salman Nazeer <salmannazeer477@gmail.com>
2026-03-07 15:50:15 +0530

    Further shortened insertion sort

--- 07_insertion_sort.c
+++ 07_insertion_sort.c
@@ -8,8 +8,7 @@ int main(){
     for(int i = 1; i < size; i++){
         int key = arr[i], j = i - 1;
         while(j >= 0 && arr[j] > key){
-            arr[j + 1] = arr[j];
-            j = j - 1;
+            arr[j + 1] = arr[j--];
         }
         arr[j + 1] = key;
     }
