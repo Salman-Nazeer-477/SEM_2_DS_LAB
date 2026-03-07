3daaea0b41cc76a7bc88a1c301c95eb7dfc59ebd
Salman Nazeer <salmannazeer477@gmail.com>
2026-03-07 15:24:43 +0530

    shortened matrix multiplication

--- 10_matrix_multiplication.c
+++ 10_matrix_multiplication.c
@@ -1,10 +1,6 @@
 #include<stdio.h>
-
 int main(){
-    int A[50][50];
-    int B[50][50];
-    int C[50][50];
-    int rA, cA, rB, cB;
+    int A[50][50], B[50][50], C[50][50], rA, cA, rB, cB;
     printf("Enter no. of rows and columns of matrix A:");
     scanf("%d %d", &rA, &cA);
     printf("Enter no. of rows and columns of matrix B:");
@@ -14,26 +10,17 @@ int main(){
         return 1;
     }
     printf("Enter Matrix A:\n");
-    for(int i = 0; i < rA; i++)
-        for(int j = 0; j < cA; j++)
-            scanf("%d", &A[i][j]);
+    for(int i = 0; i < rA; i++) for(int j = 0; j < cA; j++) scanf("%d", &A[i][j]);
     printf("Enter Matrix B:\n");
-    for(int i = 0; i < rB; i++)
-        for(int j = 0; j < cB; j++)
-            scanf("%d", &B[i][j]);
-    for(int i = 0; i < rA; i++){
+    for(int i = 0; i < rB; i++) for(int j = 0; j < cB; j++) scanf("%d", &B[i][j]);
+    for(int i = 0; i < rA; i++)
         for(int j = 0; j < cB; j++){
             C[i][j] = 0;
-            for(int k = 0; k < rB; k++){
-                C[i][j] += A[i][k] * B[k][j];
-            }
+            for(int k = 0; k < rB; k++) C[i][j] += A[i][k] * B[k][j];
         }
-    }
     printf("Matrix C = A * B:\n");
     for(int i = 0; i < rA; i++){
-        for(int j = 0; j < cB; j++){
-            printf("%d ", C[i][j]);
-        }
+        for(int j = 0; j < cB; j++) printf("%d ", C[i][j]);
         printf("\n");
     }
     return 0;
