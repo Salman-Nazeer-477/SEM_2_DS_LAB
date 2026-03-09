94b49801bb8ef734dde62f1c309befc2b9f4ecd3
Salman <salmannazeer477@gmail.com>
2026-03-09 10:40:00 +0530

    Added code for queue basic operations

--- 13_queue_using_array.c
+++ 13_queue_using_array.c
@@ -3,15 +3,27 @@
 int queue[SIZE];
 int front = -1;
 int rear = -1;
+void display();
 void enqueue(int);
 int dequeue();
 int main(){
-    enqueue(3);
-    enqueue(4);
-    display(front, rear);
-    dequeue(front, rear);
-    display(front, rear);
-    return 0;
+    int choice, num;
+    do{
+        printf("\n");
+        printf("1. Enqueue\n2. Dequeue\n3. Display\n0. Exit\n");
+        scanf("%d", &choice);
+        if(choice == 1){
+            printf("Enter number:");
+            scanf("%d", &num);
+            enqueue(num);
+        }
+        else if(choice == 2)
+            dequeue();
+        else if(choice == 3){
+            printf("Queue:");
+            display();
+        }
+    } while (choice != 0);
     
 }
 void enqueue(int num){
@@ -20,12 +32,12 @@ void enqueue(int num){
         return;
     }
     if(front == -1) front = 0;
-    rear++;
+    rear = rear + 1;
     queue[rear] = num;
 }
 
 int dequeue(){
-    if(front == rear){
+    if(front > rear || front == -1){
         printf("Queue is empty!");
         return 1;
     }
@@ -33,11 +45,11 @@ int dequeue(){
     front++;
 }
 
-void display(int front, int rear){
-    if(front == rear){
+void display(){
+    if(front > rear || front == -1){
         printf("Queue is empty");
         return;
     }
-    for(int i = front; i < rear; i++) printf("%d", queue[i]);
+    for(int i = front; i <= rear; i++) printf("%d ", queue[i]);
     printf("\n");
 }
