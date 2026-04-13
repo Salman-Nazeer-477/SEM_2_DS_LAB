d16601132fbce9e3d8ef8597556891bf5b60488e
Salman Nazeer <salmannazeer477@gmail.com>
2026-04-13 15:39:28 +0530

    modified 17th

--- 17_circular_linked_list.c
+++ 17_circular_linked_list.c
@@ -1,95 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
-struct node
-{
-    int info;
-    struct node *next;
+struct Node {
+    int data;
+    struct Node *next;
 };
-struct node *last = NULL;
-void insertAtFront(int data)
-{
-    struct node *temp = (struct node *)malloc(sizeof(struct node));
-    temp->info = data;
-    if (last == NULL)
-    {
-        temp->next = temp;
-        last = temp;
-    }
-    else
-    {
-        temp->next = last->next;
-        last->next = temp;
+struct Node *last = NULL;
+void insert(int val) {
+    struct Node *newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = val;
+    if (last == NULL) {
+        last = newNode;
+        last->next = last;
+    } else {
+        newNode->next = last->next;
+        last->next = newNode;
+        last = newNode;
     }
 }
-void insertAtEnd(int data)
-{
-    struct node *temp = (struct node *)malloc(sizeof(struct node));
-    temp->info = data;
-    if (last == NULL)
-    {
-        temp->next = temp;
-        last = temp;
-    }
-    else
-    {
-        temp->next = last->next;
-        last->next = temp;
-        last = temp;
-    }
-}
-void deleteFirst()
-{
-    if (last == NULL)
+void display() {
+    if (last == NULL) {
+        printf("List is empty\n");
         return;
-    struct node *temp = last->next;
-    if (last->next == last)
-        last = NULL;
-    else
-        last->next = temp->next;
-    free(temp);
-}
-void viewList()
-{
-    if (last == NULL)
-        return;
-    struct node *temp = last->next;
-    do
-    {
-        printf("%d ", temp->info);
+    }
+    struct Node *temp = last->next;
+    printf("List: ");
+    do {
+        printf("%d -> ", temp->data);
         temp = temp->next;
     } while (temp != last->next);
-    printf("\n");
+    printf("(back to start)\n");
 }
-int main()
-{
-    int choice;
-    do
-    {
-        printf("\n");
-        printf("1. Insert at start\n2. Insert at end\n3. Delete at start\n4. Display\n0. Exit\n");
-        scanf("%d", &choice);
-        if (choice == 1)
-        {   
-            int num;
-            printf("Enter number:");
-            scanf("%d", &num);
-            insertAtFront(num);
-        }
-        else if (choice == 2)
-        {
-            int num;
-            printf("Enter number:");
-            scanf("%d", &num);
-            insertAtEnd(num);
-        }
-        else if (choice == 3)
-        {
-            deleteFirst();
-        }
-        else if (choice == 4)
-        {
-            viewList();
-        }
-    } while (choice != 0);
+int main() {
+    insert(10);
+    insert(20);
+    insert(30);
+    insert(40);
+    display();
     return 0;
 }
